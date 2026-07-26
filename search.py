64c098789e1ae71b24341e3d47615c878ee4066d
roridemonslay <olaniyideborah63@gmail.com>
2026-07-26 11:36:06 -0400

    cosine work

--- search.py
+++ search.py
@@ -5,6 +5,16 @@ def gap_sum(v1, v2): #v1 is the query and v2. is the item the v1 and v2 stuff ar
         total += gap
     return total 
 
+def dot(v1, v2):
+    walk = 0
+    for i in range(len(v1)):
+        gap = (v1[i] * v2[i])
+        walk += gap 
+    return walk
+
+def magnitude(v):
+    
+
 def search(query,catalog, k=3):
     result = [] #the scores go here
     for item in catalog: # goes to each catalog entry
